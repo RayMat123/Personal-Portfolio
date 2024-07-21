ca74ec65c9c13ffeefd088e95d2925fd2493a84c
RayMat123 <rffshahzad@gmail.com>
2024-07-22 00:36:35 +0500

    Initial Commit

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+//Popup For in Development Projects
+document.addEventListener("DOMContentLoaded", function() {
+  // Function to show the popup
+  function showPopup() {
+    document.querySelector(".popup").style.display = "block";
+    document.querySelector(".popup-overlay").style.display = "block";
+  }
+
+  // Function to hide the popup
+  function hidePopup() {
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup-overlay").style.display = "none";
+  }
+
+  // Add event listeners to the buttons
+  document.getElementById("btn btn-color-2 project-btn").addEventListener("click", showPopup);
+
+  // Add event listener to the overlay to hide the popup when clicked
+  document.querySelector(".popup-overlay").addEventListener("click", hidePopup);
+});
